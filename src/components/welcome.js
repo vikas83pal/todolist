1a880a6e773e26ac5ae35b7771d59e01ed66456b
VIKAS PAL <110290006+vikas83pal@users.noreply.github.com>
2024-05-30 21:20:41 +0530

    Update welcome.js

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,7 +6,7 @@ const welcome = () => {
     <>
     <div className="container mt-5" align="center">
             <h1>Welcome to the Todo List App</h1>
-            <p>This is a simple Todo List application built with React. You can add, edit, and delete your todo items.</p>
+            <p>This is a simple Todo List application built with React and BootStrap. You can add, edit, and delete your todo items.</p>
             <p>To start the app navigate to home page.</p>
             <p>&copy; 2024. All rights reserved.</p>
         </div>
